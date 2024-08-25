ceb5e71ae1a14dd0c5f06670c759ca6411d58da5
Balal Arast <arastbalal@gmail.com>
2024-08-25 14:21:02 +0330

    Add Download function in laravel media trait

--- src/Laravel/Traits/Media.php
+++ src/Laravel/Traits/Media.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 trait Media
 {
-    public function upload(UploadedFile|null $file, string $path, string $disk_name = 'public') : array|null
+    public function upload(UploadedFile|null $file, string $path, string $disk_name = 'public'): ?array
     {
         if($file instanceof UploadedFile && $file->isValid()) {
             $file_ext = strtolower($file->getClientOriginalExtension());
@@ -36,5 +36,15 @@ trait Media
         }
 
         return null;
+    }
+
+    protected function download(string $path, string $disk_name = 'public'): ?string
+    {
+        if (!Storage::disk($disk_name)->exists($path)) {
+            return null;
+        }
+
+        $mime = Storage::mimeType($path) ?: 'application/octet-stream';
+        return 'data:' . $mime . ';base64,' . base64_encode(Storage::disk($disk_name)->get($path));
     }
 }
